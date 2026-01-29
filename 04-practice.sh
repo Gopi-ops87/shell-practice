ddf8f662bd4e6b8628ce544c9662529e890e6475
gopi-ops-87 <gopipasham2000@gmal.com>
2026-01-29 11:03:25 +0530

    Practice

---
+++ 04-practice.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=John
+PERSON2=Gopi
+
+echo "$PERSON1:: Hi $PERSON2 How are you?"
+echo "$PERSON2:: Hello $PERSON1, I am fine. How are you?"
+echo "$PERSON1:: what are you doing"
