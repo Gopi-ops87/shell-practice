241d35c86f058d377b076c7a7865fcea6680a71c
gopi-ops-87 <gopipasham2000@gmal.com>
2026-01-30 10:58:15 +0530

    Argument varaibles

--- 09-spe-variable.sh
+++ 09-spe-variable.sh
@@ -2,3 +2,7 @@
 
 echo "All variables passed to the script: $@"
 echo "All variables passed to the script: $*"
+echo "All variables passed to the script: $0"
+echo "All variables passed to the script: $PWD"
+echo "All variables passed to the script: $USER"
+echo "All variables passed to the script: $HOME"
