f7f5436f1efd5a6598672b6694b65af160631bee
gopi-ops-87 <gopipasham2000@gmal.com>
2026-01-30 10:28:56 +0530

    Argument varaibles

--- 06-read-variable.sh
+++ 06-read-variable.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+# echo "Please enter your pin number"
+# read PIN_NUMBER  # here PIN_NUMBER is variable
+
+# echo "PN entered is: $PIN_NUMBER"
+
 echo "Please enter your pin number"
-read PIN_NUMBER
+read -s PIN_NUMBER  # here if e use -s then pin number will be hided.
 
 echo "PN entered is: $PIN_NUMBER"
+
