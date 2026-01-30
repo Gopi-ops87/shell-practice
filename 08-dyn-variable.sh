68716b4875612e39b39823df81b0cb81aa6557ab
gopi-ops-87 <gopipasham2000@gmal.com>
2026-01-30 10:51:52 +0530

    Argument varaibles

--- 08-dyn-variable.sh
+++ 08-dyn-variable.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-DATE=$(date)
+# DATE=$(date)
 
-echo "Timestamp executed: $DATE"
+# echo "Timestamp executed: $DATE"
+
+START_TIME=$(date +%S)
+
+sleep 10
+
+END_TIME=$(date +%S)
+
+TOTAL_TIME=$((START_TIME - END_TIME))
+
+echo "script executed in: $TOTAL_TIME seconds"
