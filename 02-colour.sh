4d43554ee7791191de3a981688061261967a71bd
gopi-ops-87 <gopipasham2000@gmal.com>
2026-02-01 12:14:05 +0530

    colour code

---
+++ 02-colour.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+echo -e "$G Hello world $N"  #If we use $N then white colour will be continued after Hello world statement.
+echo "check this colour"
