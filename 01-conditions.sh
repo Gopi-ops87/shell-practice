7dc680c79479000d3aa56076ff3756d76d78e986
gopi-ops-87 <gopipasham2000@gmal.com>
2026-02-01 11:23:35 +0530

    frst condition

---
+++ 01-conditions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+NUMBER=$1
+
+if [ $NUMBER -lt 10 ]
+    echo "Given number $NUMBER is less than 10"
+else
+    echo "Given number $NUMBER is greater than or equal to 10"
+fi
+
+
+#less than = -lt
+#greater than = -gt
+#equal to = -eq
